4ce14d43b5c7b49ccbe155c1a928c000bfa55bca
Stephen Burrows <stephen.burrows@outlook.com>
2018-12-31 12:59:52 +0000

    got routing working as i want all in the one module moved css into  public

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ const path = require('path');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
 const port = process.env.port || 3000;
-// const navRoutes = require('./routes\navigation.js')
-
-const poo = require('./routes/navigation.js');
+const poo = require('./routes/navigation');
 
 app.set('view engine', 'ejs');
 
@@ -17,26 +15,8 @@ app.use(express.urlencoded({ extended: false }));
 
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(poo);
 
-app.get('/', poo);
-app.get('/index', poo);
-app.get('/about', poo);
-app.get('/team', poo);
-app.get('/conect', poo);
-app.get('/service', poo);
-app.get('/*', poo);
-/*
-app.get('/index', (req, res) => {
-  res.render('index')
-})
-
-app.get('/about', (req, res) => {
-  res.render('about')
-})
-
-app.get('*', (req, res) => {
-  res.render('error')
-}) */
 
 app.listen(port, () => {
 	// eslint-disable-next-line no-console
