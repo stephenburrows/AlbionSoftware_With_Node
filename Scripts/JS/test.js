a5cc582e20e00206c8ca03011a1657b5816f3c21
Stephen Burrows <stephen.burrows@outlook.com>
2018-12-31 16:38:19 +0000

    removed script folder made new script folder in public made a slight alteration to load_essentials

--- Scripts/JS/test.js
+++
@@ -1,8 +0,0 @@
-console.log('poo');
-let mystring;
-let test;
-test = 363;
-test = 55;
-let mybool = true;
-mybool = true;
-test = 477730;
--- Scripts/ts/test.ts
+++
@@ -1,9 +0,0 @@
-console.log('poo') 
-
-let mystring:string;
-let test:number;
-test=363;
-test=55;
-let mybool=true;
-mybool=true;
-test=477730;
